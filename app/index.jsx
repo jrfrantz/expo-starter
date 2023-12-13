eec2a3c903e6f751f589c92cae5ef2489727851a
Jacob Frantz <jacobrossfrantz@gmail.com>
2023-12-13 18:20:50 -0500

    add linear gradient

--- app/index.jsx
+++ app/index.jsx
@@ -3,33 +3,37 @@ import { isNotCreated, useEmbeddedWallet, usePrivy } from "@privy-io/expo";
 import { useEffect } from "react";
 import { useRouter } from "expo-router";
 import ImagesGrid from "../components/ImagesGrid";
+import { LinearGradient } from "expo-linear-gradient";
 export default function Page() {
   const { user, isReady } = usePrivy();
-  const wallet = useEmbeddedWallet()
+  const wallet = useEmbeddedWallet();
   const router = useRouter();
   console.log("page");
 
   useEffect(() => {
     console.log({ user, isReady });
     if (!user & isReady) {
-      router.push('/modal')
+      router.push("/modal");
     }
   }, [user, isReady]);
 
   useEffect(() => {
     if (isReady && user && isNotCreated(wallet)) {
       console.log("creating");
-      wallet.create()
+      wallet.create();
     }
-  }, [user, wallet, isReady])
+  }, [user, wallet, isReady]);
 
   return (
     <SafeAreaView>
-    <View style={{ backgroundColor: "orange", height: "100%", width: "100%" }}>
-      <Text>asdf</Text>
-      <ImagesGrid />
-      
-    </View>
+      <View
+        style={{  height: "100%", width: "100%" }}
+      >
+        <LinearGradient style={{height: '100%', width: '100%', }} colors={["#937DE530", "#50E2C830"]}>
+          <Text>asdf</Text>
+          <ImagesGrid />
+        </LinearGradient>
+      </View>
     </SafeAreaView>
   );
 }
